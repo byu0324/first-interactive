282aa450615a29ff337e184e9d5b95e93f563502
Hansae <byu0324@gmail.com>
2022-10-27 17:57:30 +0900

    desktop complete

---
+++ main.js
@@ -0,0 +1,10 @@
+function change() {
+  document.querySelector('.state').style.display = 'none';
+  document.querySelector('.thanks').style.display = 'block';
+  const value = document.querySelector('#grade').value;
+  document.querySelector('#grade').innerHTML = value;
+}
+
+function color() {
+  document.querySelector()
+}
